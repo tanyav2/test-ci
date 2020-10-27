70cb4acf574b7d02c1b492fffbce9a48cc091749
Tanya Verma <tverma@cloudflare.com>
2020-10-26 17:18:16 -0700

    test

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,13 @@
-use std::convert::TryInto;
+use reqwest::{
+    header::{HeaderMap, ACCEPT, CACHE_CONTROL, CONTENT_TYPE},
+    Client, Response,
+};
 
 fn main() {
     println!("Big if true");
-    let expected_configs = vec![
-        0, 55, 255, 3, 0, 15, 0, 32, 51, 0, 68, 86, 0, 7, 1, 32, 4, 5, 7, 8, 9, 255, 2, 0, 13,
-        0, 32, 51, 0, 68, 86, 0, 5, 1, 32, 4, 5, 7, 255, 3, 0, 15, 0, 32, 51, 0, 68, 86, 0, 7,
-        1, 32, 4, 5, 7, 8, 9,
+    let _expected_configs = vec![
+        0, 55, 255, 3, 0, 15, 0, 32, 51, 0, 68, 86, 0, 7, 1, 32, 4, 5, 7, 8, 9, 255, 2, 0, 13, 0,
+        32, 51, 0, 68, 86, 0, 5, 1, 32, 4, 5, 7, 255, 3, 0, 15, 0, 32, 51, 0, 68, 86, 0, 7, 1, 32,
+        4, 5, 7, 8, 9,
     ];
 }
